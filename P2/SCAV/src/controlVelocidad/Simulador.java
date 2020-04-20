8f4c08a1d389d8ea8b373d84c58f3cdbf6f1253d
Álex <alex-r-b@hotmail.com>
2020-04-20 17:29:25 +0200

    SCAV v2

---
+++ P2/SCAV/src/controlVelocidad/Simulador.java
@@ -0,0 +1,52 @@
+package controlVelocidad;
+
+import controlVelocidad.Filtros.FiltroCalcularRPM;
+import controlVelocidad.Filtros.FiltroRozamiento;
+import GUI.Salpicadero;
+import GUI.Ventana;
+import GUI.Controles;
+import GUI.MonitorMecanico.MonitorMecanico;
+import java.io.IOException;
+
+/**
+ *
+ * @author pablorobles
+ */
+public class Simulador {
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     */
+    public static void main(String[] args) throws IOException {
+
+        Objetivo motor = new Objetivo();
+        GestorFiltros gestor = new GestorFiltros(motor);
+        Cliente controlador = new Cliente(gestor);
+        
+        /****/
+        
+        FiltroCalcularRPM filtro_velocidad = new FiltroCalcularRPM();
+        FiltroRozamiento filtro_rozamiento = new FiltroRozamiento();
+        gestor.setFiltro(filtro_velocidad);
+        gestor.setFiltro(filtro_rozamiento);
+        
+        /****/
+        
+        Salpicadero salpicadero = new Salpicadero();
+        salpicadero.setObjetivo(motor);
+        salpicadero.setLocation(200, 200);
+        Ventana ventana_salpicadero = new Ventana(salpicadero, "Salpicadero");
+        
+        /****/
+        
+        Controles controles = new Controles();
+        controles.setControlador(controlador);
+        controles.setLocation(200, 400);
+        Ventana ventana_controles = new Ventana(controles, "Controles");
+        
+        /****/
+        MonitorMecanico monitor = new MonitorMecanico();
+        monitor.setObjetivo(motor);
+        Ventana ventana_monitor = new Ventana(monitor, "Monitor Mecánico");
+    }
+}
